8c7a833a515e4471e0528c3240eb014828371c14
Wenbin Guo <guowenbin90@gmail.com>
2021-04-21 11:31:33 -0400

    Update filter.py

--- filter.py
+++ filter.py
@@ -1,3 +1,19 @@
 import pandas as pd
 import numpy as np
 from openpyxl.workbook import Workbook
+
+#print(df)
+#print(df.loc[(df['City']=='Riverside') & (df['First']=='John')])
+
+df['Tax %'] = df['Income'].apply(lambda x: 0.15 if 10000< x < 40000 else 0.2 if 40000 < x < 80000 else .25)
+
+
+df['Tax owned'] = df['Income']*df['Tax %']
+#print(df)
+to_drop = ['Area code', 'First', 'Address']
+df.drop(columns = to_drop, inplace = True)
+#print(df)
+
+df['Test col'] = False
+df.loc[df['Income']<60000, 'Test col'] = True
+print(df.groupby(['Test col']).mean().sort_values('Income'))
