b86a2b7d9d1f1ba0a5f9bba29e25f35e7c2f6553
Wenbin Guo <guowenbin90@gmail.com>
2021-04-30 17:34:16 -0400

    Update cleaning.py

--- cleaning.py
+++ cleaning.py
@@ -1,2 +1,17 @@
-import pandas as pd
-import numpy as np
+import pandas as pd 
+import numpy as np 
+from openpyxl.workbook import Workbook
+
+df = pd.read_csv('Names.csv', header=None)
+df.columns = ['First','Last','Address','City','State','Area Code','Income']
+
+df.drop(columns='Address',inplace=True)
+
+df = df.set_index('Area Code')
+print(df.loc[8074:, 'First'])
+
+df.First = df.First.str.split(expand=True)
+
+df = df.replace(np.nan,'N/A',regex=True)
+to_excel = df.to_excel('modified.xlsx')
+print(df)
